93b7950437da98432dc76aef9d0a6984b9e0694f
elcgithub <37487676+elcgithub@users.noreply.github.com>
2018-06-28 14:48:46 -0700

    Portfolio files!!

---
+++ portfolio.js
@@ -0,0 +1,24 @@
+
+function openSection(evt, tabName) {
+	var i, tabcontent, tablinks;
+
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", " ");
+	}
+
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " active";
+}
+
+document.getElementById("defaultOpen").click();
+
+function boredFunction() {
+  var gif = document.getElementById("gif_1");
+  gif.style.display = "block";
+}
